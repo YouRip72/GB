b03f91ea3cd2720a620481a9da0800290c8e00d5
vladc_homework_1 <vlad.chmykhin@gmail.com>
2021-10-15 12:16:47 +0300

    homework_1

---
+++ Homework/3.py
@@ -0,0 +1,8 @@
+uzer_number_str = input('Введите число:' )
+number_2 = uzer_number_str*2
+number_3 = uzer_number_str*3
+uzer_number = int(uzer_number_str)
+uzer_number_2 = int(number_2)
+uzer_number_3 = int(number_3)
+sum_num = uzer_number + uzer_number_2 + uzer_number_3
+print(sum_num)
